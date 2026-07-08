4194f1ed2d8910bdc6315dfe9df763c4887770e4
harshita <harshita.maheshwari@glsuniversity.ac.in>
2026-07-08 11:26:13 +0530

    jul8

---
+++ Day4_Jul8/stringobject.js
@@ -0,0 +1,12 @@
+let str = "hello" //string 
+let str1 = new String("hello world! have a nice ! Day") //String object
+// console.log(str.padEnd(15,"*"))
+// let strarr = str1.split(" ")
+// console.log(strarr)
+// console.log(strarr[4]); //nice
+// let strarr = str1.split("!")
+// console.log(strarr)
+
+let strarr = str1.split(" ",2)
+// *****hello
+// hello+++++
